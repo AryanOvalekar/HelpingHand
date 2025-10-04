a2f61f6d115aaf964e78208143975fbfd8bd8600
AryanOvalekar <aryanovalekar@gmail.com>
2025-10-04 19:29:33 -0400

    ui enhancements

--- frontend/src/pages/ListView.jsx
+++ frontend/src/pages/ListView.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import './ListView.css'
 
 const ListView = () => {
-  return <div>list</div>
+  return (
+    <div className="listview">
+      <div className="listview-content">
+        list
+      </div>
+    </div>
+  )
 }
 
 export default ListView
