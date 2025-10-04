6c1c965c8b884cb56c2af355ba1a60e4a32dac90
AryanOvalekar <aryanovalekar@gmail.com>
2025-10-04 16:25:45 -0400

    Homepage Styles

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Homepage.css'
 
 const Homepage = () => {
-  return <div>homepage</div>
+  return (
+    <div className="homepage">
+      <h1 className="homepage-title">Name Here</h1>
+      <p className="homepage-description">
+        Helping the community, one <span className="gradient-text">step</span> at a time
+      </p>
+      <div className="homepage-buttons">
+        <Link to="/list" className="homepage-button">Start Helping</Link>
+        <Link to="/map" className="homepage-button-outline">View Map</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Homepage
