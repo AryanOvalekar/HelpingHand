85969f00efc72f41370a2a2e96557e87ea4da33b
sidesbutgithub <SidneyWai3@gmail.com>
2025-10-04 19:55:54 -0400

    added desired gemini classifications and parsing/filtering

--- grabber/grabber.py
+++ grabber/grabber.py
@@ -27,17 +27,29 @@ geminiClient = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 wordList = grabutil.create_word_list("news_keywords.json", 500)
 
 response = newsClient.get_everything(q=wordList, sort_by="relevancy", domains="")
-#print(response["totalResults"])
-
+print(response["totalResults"])
+goodResponses = [curr for curr in response['articles'] if type(curr) == dict and "title" in curr]
 #upload_article_to_mongo(article_to_db_entry(response["articles"][0]), mongoClient)
-print(response["articles"])
+titles = [curr['title'] for curr in goodResponses]
+print(len(titles))
+response = geminiClient.models.generate_content(
+    model="gemini-2.5-pro",
+    contents=f"For each of the following lines, please determine if it is a situation where volunteers or donations of money, food, or clothes are required to help people in need. If they match the specified criteria, output a new line in the format <need> <category> <severity> <location>. need is 'food', 'clothing', 'money' or 'volunteers' based on what the situation seems to require the most. category is the type of situation the title is among 'naturalDisaster', 'warRelief', 'volunteerWork'. severity is the estimated severity of the situation and urgency of the aid, either 'low' or 'high', be very strict with what you determine as high severity. location is the estimated Country, State, and City of the event based on the line. If the line does not match the specification return 'N/A'. Do not output any other words or text as part of your response, only the previously sepcified output. Here are the lines each of which for you to classify:\n {"\n".join(titles)}",
+    config=types.GenerateContentConfig(
+    )
+)
+print(response.text)
+data = response.text.split('\n')
+total = []
+goodRespCount = min(len(data), len(titles))
+for i in range(goodRespCount):
+    if data[i] != 'N/A':
+        curr = data[i].split(' ')
+        goodResponses[i]['need'] = curr[0]
+        goodResponses[i]['category'] = curr[1]
+        goodResponses[i]['severity'] = curr[2]
+        goodResponses[i]['location'] = " ".join(curr[3:])
+        total.append(goodResponses[i])
 
-# response = geminiClient.models.generate_content(
-#     model="gemini-2.5-flash-lite",
-#     contents="This is a test. Say hello to the terminal!",
-#     config=types.GenerateContentConfig(
-#         thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
-#     )
-# )
-
-# print(response.text)
+print(total)
+print(len(total))
