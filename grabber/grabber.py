36f5279ed06786d25586fb227c7c0495aae7db01
Sandwall <17184361+Sandwall@users.noreply.github.com>
2025-10-04 18:09:47 -0400

    grabber changes

--- grabber/grabber.py
+++ grabber/grabber.py
@@ -17,11 +17,6 @@ newsClient = NewsApiClient(api_key = os.getenv("NEWS_API_KEY"))
 mongoClient = pymongo.MongoClient(os.getenv("MONGO_URI"))
 geminiClient = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-keywords = []
-with open("news_keywords.json") as keywordFile:
-    keyJson = json.load(keywordFile)
-    keywords = [word for word in keyJson["include"]]
-
 #def add_newsapi_to_mongodb()
 
 # while True:
@@ -29,17 +24,20 @@ with open("news_keywords.json") as keywordFile:
 #     print(req.text)
 #     time.sleep(60)
 
-# wordList = grabutil.create_word_list(keywords, 500)
+wordList = grabutil.create_word_list("news_keywords.json", 500)
 
-# response = newsClient.get_everything(q=wordList)
+response = newsClient.get_everything(q=wordList, sort_by="relevancy", domains="")
+print(response["totalResults"])
+
+#upload_article_to_mongo(article_to_db_entry(response["articles"][0]), mongoClient)
 # print(response["articles"])
 
-response = geminiClient.models.generate_content(
-    model="gemini-2.5-flash-lite",
-    contents="This is a test. Say hello to the terminal!",
-    config=types.GenerateContentConfig(
-        thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
-    )
-)
+# response = geminiClient.models.generate_content(
+#     model="gemini-2.5-flash-lite",
+#     contents="This is a test. Say hello to the terminal!",
+#     config=types.GenerateContentConfig(
+#         thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
+#     )
+# )
 
-print(response.text)
+# print(response.text)
